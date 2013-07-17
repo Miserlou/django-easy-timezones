b18474799efbce55c621d8f461ed601b98f7f339
Rich Jones <rich@anomos.info>
2013-07-16 23:00:22 -0700

    setup py begin

---
+++ easy-timezones/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-easy-timezones',
+    version='0.1',
+    packages=['easy-timezones'],
+    include_package_data=True,
+    license='Apache License',  # example license
+    description='Easy timezones for Django (>=1.4) based on MaxMind GeoIP.',
+    long_description=README,
+    url='https://github.com/Miserlou/django-easy-timezones',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
