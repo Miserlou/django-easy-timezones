2bbedf192fb6c7591ee651813f96b0ad46a4bd72
Bill Blanchard <bill@plumbdev.com>
2015-12-29 12:55:52 -0300

    Add support for Django 1.9 and Python 3.4

--- test_settings.py
+++ test_settings.py
@@ -1,3 +1,7 @@
+import django
+DJANGO_18_PLUS = django.VERSION[:2] >= (1, 8)
+DJANGO_18_PLUS_APPS = ['django.contrib.auth', 'django.contrib.contenttypes']
+DJANGO_APPS_ALL_VERSIONS = ['django.contrib.sessions', 'easy_timezones']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -5,11 +9,16 @@ DATABASES = {
     }
 }
 SECRET_KEY = "secret_key_for_testing"
-INSTALLED_APPS = ['django.contrib.sessions', 'easy_timezones']
 MIDDLEWARE_CLASSES = ['django.contrib.sessions.middleware.SessionMiddleware', 'easy_timezones.middleware.EasyTimezoneMiddleware']
 GEOIP_DATABASE = 'GeoLiteCity.dat' 
 ROOT_URLCONF = 'easy_timezones.urls'
+DEBUG = True
 TIME_ZONE = 'UTC'
 ALLOWED_HOSTS = ["*"]
-DEBUG = True
 AUTH_USER_MODEL = None
+
+if DJANGO_18_PLUS:
+	INSTALLED_APPS = DJANGO_18_PLUS_APPS + DJANGO_APPS_ALL_VERSIONS
+	TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates'}]
+else:
+	INSTALLED_APPS = DJANGO_APPS_ALL_VERSIONS
