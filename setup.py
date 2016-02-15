57145456cbef3bf3bdda5b1f537b20e71fde36d0
Rich Jones <rich@anomos.info>
2016-02-15 01:45:22 +0100

    Lets see what happens in P3..

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
